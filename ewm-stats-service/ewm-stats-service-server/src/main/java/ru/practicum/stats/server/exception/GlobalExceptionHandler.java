f6407c3990f4d0c5bae22f04e5bd9a4256282ff1
acoent <153868119+acoent@users.noreply.github.com>
2025-10-25 21:10:19 -0500

    fix: clean up stats logging and  event updates

--- ewm-stats-service/ewm-stats-service-server/src/main/java/ru/practicum/stats/server/exception/GlobalExceptionHandler.java
+++ ewm-stats-service/ewm-stats-service-server/src/main/java/ru/practicum/stats/server/exception/GlobalExceptionHandler.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -43,7 +44,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler({InvalidDateRangeException.class, MissingRequiredParameterException.class, DateTimeParseException.class,
-            MethodArgumentTypeMismatchException.class, HttpMessageNotReadableException.class})
+            MethodArgumentTypeMismatchException.class, HttpMessageNotReadableException.class, MissingServletRequestParameterException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponse handleBadRequest(RuntimeException ex) {
         log.warn("Bad request: {}", ex.getMessage());
